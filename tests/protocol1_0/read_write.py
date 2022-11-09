f78dc2ee67423a7a900ec01b41a06e67b31b5ce9
Siwakon <tiger.prasang@gmail.com>
2022-11-09 17:23:44 +0700

    Update

---
+++ tests/protocol1_0/read_write.py
@@ -0,0 +1,58 @@
+
+
+import os
+import rospy
+from dynamixel_sdk import *
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+# Control table address
+ADDR_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
+ADDR_GOAL_POSITION      = 116
+ADDR_PRESENT_POSITION   = 132
+
+# Protocol version
+PROTOCOL_VERSION            = 1.0               # See which protocol version is used in the Dynamixel
+
+# Default setting
+DXL_ID                      = 1                 # Dynamixel ID : 1
+BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
+DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+TORQUE_ENABLE               = 1                 # Value for enabling the torque
+TORQUE_DISABLE              = 0                 # Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = 0               # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 1000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
+
+portHandler = PortHandler(DEVICENAME)
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+def set_goal_pos_callback(data):
+    print("Set Goal Position of ID %s = %s" % (data.id, data.position))
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, data.id, ADDR_GOAL_POSITION, data.position)
+
+def get_present_pos(dxl_id):
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, dxl_id, ADDR_PRESENT_POSITION)
+    print("Present Position of ID %s = %s" % (dxl_id, dxl_present_position))
+    return dxl_present_position
+
+
+
+if __name__ == '__main__':
+    get_present_pos(dxl_id=1)
